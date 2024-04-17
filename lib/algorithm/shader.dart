c3b1afd112b53e8fa4af8bbe3719d4a0724468d4
annomy <annomy634@gmail.com>
2024-04-17 11:57:25 +0545

    added shader for ui enhancement

---
+++ lib/algorithm/shader.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Shadow universalShadow() {
+  return const Shadow(color: Colors.grey, offset: Offset(2, 2), blurRadius: 5);
+}
--- lib/elements/label.dart
+++ lib/elements/label.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:food_2_hunger/algorithm/shader.dart';
 
-Widget titleLabel(BuildContext context) {
-  return const Text(
-    "Food 2 Hungry",
-    textAlign: TextAlign.center,
-    style: TextStyle(fontFamily: 'joti', fontSize: 36, color: Colors.green),
-  );
+Widget titleLabel(BuildContext context,
+    {double ssize = 36, TextAlign align = TextAlign.center}) {
+  return Text("Food 2 Hungry",
+      textAlign: align,
+      style: TextStyle(
+          fontFamily: 'joti',
+          fontSize: ssize,
+          color: Theme.of(context).colorScheme.surface,
+          shadows: [universalShadow()]));
 }
